81f6def3090268d07b15ba5a0942aed13b841697
vmilovanovicc <codingvesna1@gmail.com>
2022-10-17 20:23:44 +0200

    Basic service package - start the server, register the handlers, shutdown

---
+++ service/service.go
@@ -0,0 +1,38 @@
+package service
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// kick off web service
+func Start(ctx context.Context, serviceName, host, port string, registerHandlersFunc func()) (context.Context, error) {
+	registerHandlersFunc()
+	ctx = startService(ctx, serviceName, host, port)
+
+	return ctx, nil
+}
+
+func startService(ctx context.Context, serviceName, host, port string) context.Context {
+	ctx, cancel := context.WithCancel(ctx)
+
+	// TODO: create instance of a server
+	var srv http.Server
+	srv.Addr = ":" + port
+	// TODO: create go routines - start listening and allow the option to shutdown
+	go func() {
+		log.Println(srv.ListenAndServe())
+		cancel()
+	}()
+
+	go func() {
+		fmt.Printf("%v started. Press any key to stop.\n", serviceName)
+		var s string
+		fmt.Scanln(&s)
+		srv.Shutdown(ctx)
+		cancel()
+	}()
+	return ctx
+}
