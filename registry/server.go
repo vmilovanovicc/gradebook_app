6d56fb903a513cadd3e85a64a22109d9d8080f29
vmilovanovicc <codingvesna1@gmail.com>
2022-10-29 15:29:44 +0200

    Create the registry service

--- registry/server.go
+++ registry/server.go
@@ -1,6 +1,11 @@
 package registry
 
-import "sync"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"sync"
+)
 
 // Define the registry service
 
@@ -27,4 +32,32 @@ func (r *registry) add(reg Registration) error {
 var reg = registry{
 	registrations: make([]Registration, 0),
 	mutex:         new(sync.Mutex),
+}
+
+type RegistryService struct{}
+
+// Accept a new registration.
+func (s RegistryService) ServerHTTP(w http.ResponseWriter, req *http.Request) {
+	log.Println("Request received.")
+	switch req.Method {
+	case http.MethodPost:
+		dec := json.NewDecoder(req.Body)
+		var r Registration
+		err := dec.Decode(&r)
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		log.Printf("Adding a service: %v with URL: %v\n", r.ServiceName, r.ServiceURL)
+		err = reg.add(r)
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
 }
