76f53704346534cd745fda5d570badc810b38a0b
vmilovanovicc <codingvesna1@gmail.com>
2022-11-12 14:22:35 +0100

    Define patterns which Grading service covers

---
+++ grades/server.go
@@ -0,0 +1,44 @@
+package grades
+
+import (
+	"net/http"
+	"strconv"
+	"strings"
+)
+
+// define server logic
+func RegisterHandlers() {
+	handler := new(studentsHandler)
+	http.Handle("/students", handler)
+	http.Handle("/students/", handler)
+}
+
+type studentsHandler struct{}
+
+// ServeHttp handles a couple of patterns:
+// /students - entire class ---> 0 empty, 1 students
+// /students/{id} - a single student's record ---> 0 empty, 1 students, 2 id
+// /students/{id}/grades - a single student's grades ---> 0 empty, 1 students, 2 id, 3 grades
+func (sh studentsHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	pathSegments := strings.Split(req.URL.Path, "/")
+	switch len(pathSegments) {
+	case 2:
+		sh.getAll(w, req)
+	case 3:
+		id, err := strconv.Atoi(pathSegments[2])
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+		sh.getOne(w, req, id)
+	case 4:
+		id, err := strconv.Atoi(pathSegments[2])
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+		sh.addGrade(w, req, id)
+	default:
+		w.WriteHeader(http.StatusNotFound)
+	}
+}
