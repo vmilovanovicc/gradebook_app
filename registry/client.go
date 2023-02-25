62af98beabcb6264dcb0cfaec3f80419896d948f
vmilovanovicc <codingvesna1@gmail.com>
2023-02-25 11:08:41 +0100

    Setup handler to receive messages from the registry service

--- registry/client.go
+++ registry/client.go
@@ -4,14 +4,22 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"log"
 	"math/rand"
 	"net/http"
+	"net/url"
 	"sync"
 )
 
 // RegisterService allows a web service to register itself with the service registry.
 // Buffer holds data for the POST request.
 func RegisterService(r Registration) error {
+	serviceUpdateURL, er := url.Parse(r.ServiceUpdateURL)
+	if er != nil {
+		return er
+	}
+	http.Handle(serviceUpdateURL.Path, &serviceUpdateHandler{})
+
 	buf := new(bytes.Buffer)
 	enc := json.NewEncoder(buf)
 	err := enc.Encode(r)
@@ -26,6 +34,24 @@ func RegisterService(r Registration) error {
 		return fmt.Errorf("failed to register service. Registry service responded with code %v", res.StatusCode)
 	}
 	return nil
+}
+
+type serviceUpdateHandler struct{}
+
+func (suh serviceUpdateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+	dec := json.NewDecoder(r.Body)
+	var p patch
+	err := dec.Decode(&p)
+	if err != nil {
+		log.Println(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	prov.Update(p)
 }
 
 // ShutdownService allows a web service to deregister itself with the service registry.
@@ -74,7 +100,7 @@ func (p *providers) Update(pat patch) {
 func (p providers) get(name ServiceName) (string, error) {
 	providers, ok := p.services[name]
 	if !ok {
-		return "", fmt.Errorf("No providers available for service %v", name)
+		return "", fmt.Errorf("no providers available for service %v", name)
 	}
 	idx := int(rand.Float32() * float32(len(providers)))
 	return providers[idx], nil
